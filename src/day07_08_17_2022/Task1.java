7831a0aaa11a16831f9b25e3fa65ffb849088da6
Luzvin Urban <lucybdk@gmail.com>
2022-08-24 17:04:33 -0400

    Day 8 added Task1.java file

--- src/day07_08_17_2022/Task1.java
+++ src/day07_08_17_2022/Task1.java
@@ -1,7 +1,23 @@
 package day07_08_17_2022;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Task1 {
     public static void main(String[] args) {
 
     }
+/*
+ Given an ArrayList of Strings, go through and read only Strings that are character or less.
+ */
+
+
+    ArrayList<String> list = new ArrayList<>(Arrays.asList("apples","tree","loop","cat","animal","shortcut"));
+    // get the words one by one
+
+
+    // check words if its length smaller than or equal
+
+    // put the another empty list
 }
+
